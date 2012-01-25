9e0b6700c4dfc44fa7bdee566c7e3cd8d167a956
Frank Lakatos <me@fakingfantastic.com>
2012-01-25 09:02:22 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Major::Application.config.secret_token = 'ff372f8192871198e348446db7e9d1d68021775d38ea2ac94a290801d7f8d86e8b07f927c1ed70c7f4f23ca44d08a4f9c02cb773560c862abd14e59f1aba3d0c'
